d801fac9bd7869f4115c1ce7e26512106815115a
Tdxdxoz <tdxdxoz@gmail.com>
2019-10-16 15:45:55 +0800

    4 times per 600s, and do not change the name

--- tree_hole_bots/hole.py
+++ tree_hole_bots/hole.py
@@ -2,6 +2,17 @@ from mastodon import Mastodon
 import random
 import html2text
 import time
+#import pprint
+
+MAX_SEND = 4
+REC_TIME = 600
+
+def POST(msg):
+    th.status_post(msg)
+
+def PM(msg):
+    th.status_post(msg, visibility='direct')
+
 
 #   Set up Mastodon
 token = open('token.secret','r').read().strip('\n')
@@ -11,30 +22,50 @@ th = Mastodon(
     api_base_url = 'https://thu.closed.social/'
 )
 
-lid = 0
+lid = 102970936686565234
+recs = {}
 
 h2t = html2text.HTML2Text()
 h2t.ignore_links = True
+
+#pp = pprint.PrettyPrinter(indent=4)
 
 with open("THUOCL_animal.txt",'r') as x:
     names = x.readlines()
 
 while True:
-    r = th.conversations(since_id=lid);
-
+    r = th.conversations(min_id=lid);
+    
     for conv in r[::-1]:
+        print(conv.last_status.id,conv.unread, conv.last_status.account.acct)
         if conv.unread:
-            text = conv.last_status.content
-            pt = h2t.handle(text).lstrip()
+            
+            #pp.pprint(conv);
+            name = conv.last_status.account.acct
+            if '@' in name:
+                PM('树洞只允许在本站使用 @'+name)
+            else:
+                rec = recs.get(name,[])
+                cur_time = time.time()
+                print('rec',rec)
+                rec = [mes for mes in rec if cur_time - mes[0] < REC_TIME]
+                if(len(rec) >= MAX_SEND):
+                    PM('次数已满, %d 秒后可继续使用 @%s' % (int(REC_TIME + rec[0][0] - cur_time),name))
+                else:
+                    if(len(rec)):
+                        ani = rec[0][1]
+                    else:
+                        ani = random.choice(names).split()[0]
+                    rec.append((cur_time,ani))
 
-            rid = random.randint(0,len(names)-1)
-            th.status_post(names[rid].split()[0] + ":\n" + pt)
+                    text = conv.last_status.content
+                    pt = h2t.handle(text).lstrip()
+
+                    POST(ani + ":\n" + pt)
+                    recs[name] = rec
+
             th.conversations_read(conv.id)
-        lid = max(lid,conv.id);
-    print(lid)
+        lid = max(lid,conv.last_status.id);
+    #print(lid)
+    time.sleep(5)
 
-    time.sleep(10)
-
-#print(len(r))
-#pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(r)
