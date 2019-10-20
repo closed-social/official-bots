2954fb3adb8c54f0a3f95ff805373c6c6842e5b6
Tdxdxoz <tdxdxoz@gmail.com>
2019-10-20 17:18:05 +0800

    change treehole to unlimited and private, change name of namelist

--- tree_hole_bots/hole.py
+++ tree_hole_bots/hole.py
@@ -1,16 +1,29 @@
 from mastodon import Mastodon
-import random
+from numpy import random
 import html2text
 import time
-#import pprint
+import pprint
 
-MAX_SEND = 4
+MAX_SEND = -1
 REC_TIME = 600
+BOT_NAME = '@tree_hole_bot'
+DOMAIN   = 'thu.closed.social'
+PREFIX   = '幸运的 '
 
 def POST(msg):
-    th.status_post(msg)
+    if(len(msg) > 500):
+        msg = msg[:500]
+    th.status_post(msg, visibility='public')
+
+def PRIV(msg):
+    if(len(msg) > 500):
+        msg = msg[:500]
+    print('PRIV')
+    th.status_post(msg, visibility='private')
 
 def PM(msg):
+    if(len(msg) > 500):
+        msg = msg[:500]
     th.status_post(msg, visibility='direct')
 
 
@@ -19,18 +32,18 @@ token = open('token.secret','r').read().strip('\n')
 print(token)
 th = Mastodon(
     access_token = token,
-    api_base_url = 'https://thu.closed.social/'
+    api_base_url = 'https://' + DOMAIN
 )
 
-lid = 102970936686565234
+lid = 102992003218277202
 recs = {}
 
 h2t = html2text.HTML2Text()
 h2t.ignore_links = True
 
-#pp = pprint.PrettyPrinter(indent=4)
+pp = pprint.PrettyPrinter(indent=4)
 
-with open("THUOCL_animal.txt",'r') as x:
+with open("namelist",'r') as x:
     names = x.readlines()
 
 while True:
@@ -40,7 +53,7 @@ while True:
         print(conv.last_status.id,conv.unread, conv.last_status.account.acct)
         if conv.unread:
             
-            #pp.pprint(conv);
+            pp.pprint(conv);
             name = conv.last_status.account.acct
             if '@' in name:
                 PM('树洞只允许在本站使用 @'+name)
@@ -49,7 +62,7 @@ while True:
                 cur_time = time.time()
                 print('rec',rec)
                 rec = [mes for mes in rec if cur_time - mes[0] < REC_TIME]
-                if(len(rec) >= MAX_SEND):
+                if(len(rec) == MAX_SEND):
                     PM('次数已满, %d 秒后可继续使用 @%s' % (int(REC_TIME + rec[0][0] - cur_time),name))
                 else:
                     if(len(rec)):
@@ -60,8 +73,13 @@ while True:
 
                     text = conv.last_status.content
                     pt = h2t.handle(text).lstrip()
+                    pt = pt.replace(BOT_NAME,'')
 
-                    POST(ani + ":\n" + pt)
+                    wt = min(len(pt), 100)
+                    if(random.choice([True, False], p=[wt*wt/25000, 1- wt*wt/25000])):
+                        POST(PREFIX + ani + ":\n" + pt)
+                    else:
+                        PRIV(ani + ":\n" + pt)
                     recs[name] = rec
 
             th.conversations_read(conv.id)
